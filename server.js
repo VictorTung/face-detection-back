ca9b4cf068f091f5d56b4f35d2cc9846d6db8d05
Kwan Wing Tung <nffllf@pm.me>
2022-03-21 12:08:22 +0800

    try error3

--- server.js
+++ server.js
@@ -23,30 +23,26 @@ const profile = require("./controller/profile");
 // heroku
 const db = knex({
   client: "pg",
-  connection: {
-    host: "postgresql-shallow-13183",
-    port: 5432,
-    user: "postgres",
-    password: "password",
-    database: "smart-brain",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
-
-
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {res.send('work!')});
+app.get("/", (req, res) => {
+  res.send("work!");
+});
 app.get("/profile/:id", profile.handleProfile(db, bcrypt));
 app.post("/signin", signin.handleSignIn(db, bcrypt));
 app.post("/register", register.handleRegister(db, bcrypt));
 app.put("/img", img.handleImage(db, bcrypt));
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`app is running on port ${PORT}`);
 });
