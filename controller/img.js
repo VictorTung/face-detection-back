143dc099a01cec3d8e374a83358516aad627885f
Kwan Wing Tung <nffllf@pm.me>
2022-03-22 05:00:14 +0800

    fix

--- controller/img.js
+++ controller/img.js
@@ -1,4 +1,11 @@
 const Clarifai = require("clarifai");
+const { Pool } = require('pg')
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 const clarifaiApp = new Clarifai.App({
   apiKey: "2640b395f7c349148a8ce86d50ab5b98",
@@ -14,7 +21,7 @@ const handleImage = (db, bcrypt) => (req, res) => {
     .then((response) => {
       console.log(1);
       console.log(response);
-        db.query('UPDATE users SET entries = 1 WHERE id = $1 RETURNING entries;', [id], (error, entries) => {
+      pool.query('UPDATE users SET entries = 1 WHERE id = $1 RETURNING entries;', [id], (error, entries) => {
           if (entries.length) {
             console.log(entries);
             res.json({
