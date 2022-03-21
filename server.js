79cce4a17c4f3ca0703d8bd40f2b590fa84077e9
Kwan Wing Tung <nffllf@pm.me>
2022-03-21 14:20:56 +0800

    try

--- server.js
+++ server.js
@@ -8,13 +8,23 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
-  // connect to your own database here:
-  client: 'pg',
+const { Client } = require('pg');
+
+const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
 });
 
 const app = express();
