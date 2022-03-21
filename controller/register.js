e967102f6ba4eba28e7f395bb6c538249df00f78
Kwan Wing Tung <nffllf@pm.me>
2022-03-21 18:00:13 +0800

    test register

--- controller/register.js
+++ controller/register.js
@@ -1,4 +1,7 @@
-const handleRegister = (db, bcrypt) => (req, res) => {
+const { Pool } = require('pg')
+const pool = new Pool()
+
+const handleRegister = (db, bcrypt) => async (req, res) => {
   const { name, password, email } = req.body;
   const hash = bcrypt.hashSync(password);
 
@@ -7,26 +10,46 @@ const handleRegister = (db, bcrypt) => (req, res) => {
   }
 
   // insert data to database
-  db.transaction((trx) => {
-    trx
-      .insert({ email, hash })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            name: name,
-            email: email,
-            joined: new Date(),
-          })
-          .then((user) => res.json(user[0]))
-          .catch((err) => res.status(400).json("unable to get user"));
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  })
-  .catch(err=> res.status(400).json('unable to register'))
+  // db.transaction((trx) => {
+  //   trx
+  //     .insert({ email, hash })
+  //     .into("login")
+  //     .returning("email")
+  //     .then((loginEmail) => {
+  //       return trx("users")
+  //         .returning("*")
+  //         .insert({
+  //           name: name,
+  //           email: email,
+  //           joined: new Date(),
+  //         })
+  //         .then((user) => res.json(user[0]))
+  //         .catch((err) => res.status(400).json("unable to get user"));
+  //     })
+  //     .then(trx.commit)
+  //     .catch(trx.rollback);
+  // })
+  // .catch(err=> res.status(400).json('unable to register'))
+  
+  const client = await pool.connect()
+
+  try{
+    await client.query('BEGIN')
+    const loginText = `INSERT INTO login(email, hash) VALUES($1, $2) RETURNING email`
+    const loginValue = [email, password]
+    const res = await client.query(queryText, queryValue)
+
+    const userText = 'INSERT INTO users(name, email) VALUES($1, $2)'
+    const userValue = [name, res.rows[0].email]
+    const user = await client.query(userText, userValue)
+    res.json(user[0])
+    await client.query('COMMIT')
+  } catch(e){
+    await client.query('ROLLBACK')
+    throw e
+  } finally {
+    client.release()
+  }
 };
 
 module.exports = {
