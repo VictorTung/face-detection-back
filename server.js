43327fcc228d4092d29bff9dbb46f239b824cd12
Kwan Wing Tung <nffllf@pm.me>
2022-03-22 02:17:52 +0800

    fix

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const cors = require("cors");
 // const knex = require("knex");
 const { Client } = require('pg');
 
-const signin = require("./controller/signin");
-const register = require("./controller/register");
-const img = require("./controller/img");
-const profile = require("./controller/profile");
+// const signin = require("./controller/signin");
+// const register = require("./controller/register");
+// const img = require("./controller/img");
+// const profile = require("./controller/profile");
 
 
 
@@ -36,10 +36,10 @@ app.get("/all", (req, res) => {
   
   db.end();
 });
-app.get("/profile/:id", profile.handleProfile(db, bcrypt));
-app.post("/signin", signin.handleSignIn(db, bcrypt));
-app.post("/register", register.handleRegister(db, bcrypt));
-app.put("/img", img.handleImage(db, bcrypt));
+// app.get("/profile/:id", profile.handleProfile(db, bcrypt));
+// app.post("/signin", signin.handleSignIn(db, bcrypt));
+// app.post("/register", register.handleRegister(db, bcrypt));
+// app.put("/img", img.handleImage(db, bcrypt));
 
 
 const PORT = process.env.PORT || 3001
