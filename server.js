d4527f7aa4bc5515ecbadea13a1353e8509fb9ad
Kwan Wing Tung <nffllf@pm.me>
2022-03-21 14:33:35 +0800

    npm pg test

--- server.js
+++ server.js
@@ -1,44 +1,53 @@
-const express = require('express');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
+const express = require("express");
+const bcrypt = require("bcrypt-nodejs");
+const cors = require("cors");
+const knex = require("knex");
 
-const register = require('./controllers/register');
-const signin = require('./controllers/signin');
-const profile = require('./controllers/profile');
-const image = require('./controllers/image');
+// const register = require('./controllers/register');
+// const signin = require('./controllers/signin');
+// const profile = require('./controllers/profile');
+// const image = require('./controllers/image');
 
-const { Client } = require('pg');
+const { Client } = require("pg");
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
+client.query(
+  "SELECT * FROM login;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
   }
-  client.end();
-});
+);
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
 
-app.get('/', (req, res)=> { res.send(db.login) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
-app.put('/image', (req, res) => { image.handleImage(req, res, db)})
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+app.get("/", (req, res) => {
+  client
+    .query('SELECT * FROM login WHERE id = $1'. [1])
+    .then(data=> res.json(data))
+});
 
-app.listen(process.env.PORT || 3000, ()=> {
-  console.log('app is running on port process.env.PORT');
-})
+// app.get('/', (req, res)=> { res.send(db.login) })
+// app.post('/signin', signin.handleSignin(db, bcrypt))
+// app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+// app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+// app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+// app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("app is running on port process.env.PORT");
+});
