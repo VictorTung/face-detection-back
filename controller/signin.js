f10050c3c6cecfa0b8ca90540f8a5e01b1c0b406
Kwan Wing Tung <nffllf@pm.me>
2022-03-22 12:43:51 +0800

    fix

--- controller/signin.js
+++ controller/signin.js
@@ -14,7 +14,7 @@ const handleSignIn = (db, bcrypt) => (req, res) => {
         return res.status(400).json("invalid password or email");
       }
       db.query('SELECT * FROM users WHERE email = $1', [email], (error2, user) => {
-        res.append("Access-Control-Allow-Origin", "*").json(user.rows[0])
+        res.json(user.rows[0])
       })
     });
 };
