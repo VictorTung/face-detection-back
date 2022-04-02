bdbdb2a0a7a73b67d236e664c4d06fde640b0133
Kwan Wing Tung <nffllf@pm.me>
2022-04-02 23:38:03 +0800

    fix

--- controller/signin.js
+++ controller/signin.js
@@ -4,15 +4,24 @@ const handleSignIn = (db, bcrypt) => (req, res) => {
     return res.status(400).json("incorrect form submission");
   }
 
-    db.query('SELECT email, hash FROM login WHERE email = $1', [email], (error, response) => {
+  db.query(
+    "SELECT email, hash FROM login WHERE email = $1",
+    [email],
+    (error, response) => {
+      console.log(response.rows[0]);
       const isValid = bcrypt.compareSync(password, response.rows[0].hash);
-      if(!isValid){
+      if (!isValid) {
         return res.status(400).json("invalid password or email");
       }
-      db.query('SELECT * FROM users WHERE email = $1', [email], (error2, user) => {
-        res.json(user.rows[0])
-      })
-    });
+      db.query(
+        "SELECT * FROM users WHERE email = $1",
+        [email],
+        (error2, user) => {
+          res.json(user.rows[0]);
+        }
+      );
+    }
+  );
 };
 
 module.exports = {
